c66d2aee8271656a1317049c29a220c4c36a610b
louis7308 <jswa7308@naver.com>
2022-09-02 01:05:09 +0900

    :tada: :: Project Setting

---
+++ src/main/java/com/example/took_backend/TookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.took_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/took_backend/TookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.took_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
