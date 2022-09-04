af75cfbbbff772d5b1e0099aa0436e90ced70cfd
louis7308 <jswa7308@naver.com>
2022-09-04 15:57:03 +0900

    :ambulance: :: uuid varbinary(36) 으로 변경

---
+++ src/main/java/com/example/took_backend/domain/businesscard/BusinessCard.java
@@ -0,0 +1,41 @@
+package com.example.took_backend.domain.businesscard;
+
+import com.example.took_backend.domain.cardexhange.CardExchange;
+import com.example.took_backend.domain.user.User;
+import com.example.took_backend.global.entity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class BusinessCard extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "VARBINARY(36)")
+    @Builder.Default
+    private UUID uuid = UUID.randomUUID();
+
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_uuid")
+    private User user;
+
+    @OneToMany(mappedBy = "businessCard")
+    private List<CardExchange> cardExchanges = new ArrayList<>();
+}
