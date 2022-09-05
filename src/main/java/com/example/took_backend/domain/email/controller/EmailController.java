c5ebcffa517a41d28c73657871da697d99542790
Huuuunee <s21024@gsm.hs.kr>
2022-09-06 01:27:08 +0900

    :sparkles: Email 컨트롤러 생성 및 postmapping

---
+++ src/main/java/com/example/took_backend/domain/email/controller/EmailController.java
@@ -0,0 +1,23 @@
+package com.example.took_backend.domain.email.controller;
+
+import com.example.took_backend.domain.email.presentation.dto.request.EmailSentDto;
+import com.example.took_backend.domain.email.service.MailSenderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("email")
+public class EmailController {
+    private final MailSenderService mailSenderService;
+    @PostMapping
+    public ResponseEntity<Void> authEmail(@RequestBody EmailSentDto emailSentDto){
+        mailSenderService.execute(emailSentDto);
+        return ResponseEntity.ok().build();
+    }
+}
