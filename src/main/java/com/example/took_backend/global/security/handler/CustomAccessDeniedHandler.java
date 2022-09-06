e0c6ebbec85c68bb77be9b5e989da47081916458
louis7308 <jswa7308@naver.com>
2022-09-06 19:50:28 +0900

    :sparkles: :: exceptionHandler 추가

--- src/main/java/com/example/took_backend/global/security/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/took_backend/global/security/handler/CustomAccessDeniedHandler.java
@@ -1,2 +1,21 @@
-package com.example.took_backend.global.security.handler;public class CustomAccessDeniedHandler {
+package com.example.took_backend.global.security.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("============Access Denied==============");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
 }
