44ac51956c6e8f0f520f0e39dd3718c50178e418
Huuuunee <s21024@gsm.hs.kr>
2022-09-06 20:12:27 +0900

    :sparkles: 15분 안에 3회 이상 조회시 에러 추가

--- src/main/java/com/example/took_backend/global/exception/ErrorCode.java
+++ src/main/java/com/example/took_backend/global/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     UNKNOWN("알 수 없는 에러", 500),
-    AUTH_CODE_EXPIRED("메일 발송에 실패 했습니다",500);
+    AUTH_CODE_EXPIRED("메일 발송에 실패 했습니다",500),
+    MANY_REQUEST_EMAIL_AUTH("15분에 최대 3번 이메일 인증을 요청할 수 있습니다.", 429);
     private final String message;
     private final int status;
 }
