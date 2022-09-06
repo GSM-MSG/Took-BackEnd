d33904ca3a32c15738f1e3f35e44039a3817b673
louis7308 <jswa7308@naver.com>
2022-09-06 19:50:55 +0900

    :sparkles: :: exceptionhandler config 추가

--- src/main/java/com/example/took_backend/global/security/SecurityConfig.java
+++ src/main/java/com/example/took_backend/global/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.took_backend.global.security;
 
+import com.example.took_backend.global.security.handler.CustomAccessDeniedHandler;
+import com.example.took_backend.global.security.handler.CustomAuthenticationEntryPointHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -29,7 +31,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable();
-
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST,
@@ -38,10 +39,13 @@ public class SecurityConfig {
                         "/user"
                 ).permitAll()
                 .anyRequest().authenticated();
-
         http
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .exceptionHandling()
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                .authenticationEntryPoint(new CustomAuthenticationEntryPointHandler());
 
 
     }
