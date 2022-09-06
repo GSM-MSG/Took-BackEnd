9a923e6c825b90723424248cdd515567d0444d52
louis7308 <jswa7308@naver.com>
2022-09-06 23:40:47 +0900

    :sparkles: :: 토큰 만료된 ErrorCode

--- src/main/java/com/example/took_backend/global/exception/ErrorCode.java
+++ src/main/java/com/example/took_backend/global/exception/ErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 에러입니다."),
-    TOKEN_EXPIRATION(HttpStatus.UNAUTHORIZED, "토큰이 만료 되었습니다.");
+    TOKEN_EXPIRATION(HttpStatus.UNAUTHORIZED, "토큰이 만료 되었습니다."),
     TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED, "토큰이 유효 하지 않습니다.");
 
     private final HttpStatus httpStatus;
