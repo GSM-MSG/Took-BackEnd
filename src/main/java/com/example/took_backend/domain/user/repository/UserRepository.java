311ab0fb1bd9205433c397234a5d8a169537749b
louis7308 <jswa7308@naver.com>
2022-09-07 00:54:53 +0900

    :fire: :: 불필요한 어노테이션 제거

--- src/main/java/com/example/took_backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/took_backend/domain/user/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findOneByEmail(String email);
 }
