9a463e7b823f10c45b20731cb315286905cc2ec1
louis7308 <jswa7308@naver.com>
2022-09-11 20:28:22 +0900

    :ambulance: :: ErrorCode 기준으로 오름차순 정렬

--- src/main/java/com/example/took_backend/global/exception/ErrorCode.java
+++ src/main/java/com/example/took_backend/global/exception/ErrorCode.java
@@ -9,9 +9,9 @@ import lombok.Getter;
 public enum ErrorCode {
     TOKEN_EXPIRATION("토큰이 만료 되었습니다.", 401),
     TOKEN_NOT_VALID("토큰이 유효 하지 않습니다.", 401),
+    MANY_REQUEST_EMAIL_AUTH("15분에 최대 3번 이메일 인증을 요청할 수 있습니다.", 429),
     UNKNOWN("알 수 없는 에러", 500),
-    AUTH_CODE_EXPIRED("메일 발송에 실패 했습니다",500),
-    MANY_REQUEST_EMAIL_AUTH("15분에 최대 3번 이메일 인증을 요청할 수 있습니다.", 429);
+    AUTH_CODE_EXPIRED("메일 발송에 실패 했습니다",500);
     private final String message;
     private final int status;
 }
