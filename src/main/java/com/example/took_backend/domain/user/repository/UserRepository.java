4297bcb2d44418a77a4f50c6a1df22c6e6d8644e
louis7308 <jswa7308@naver.com>
2022-09-13 16:19:46 +0900

    :sparkles: :: DB에 이미 존재하는 회원인지 판별

--- src/main/java/com/example/took_backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/took_backend/domain/user/repository/UserRepository.java
@@ -3,13 +3,11 @@ package com.example.took_backend.domain.user.repository;
 import com.example.took_backend.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.stereotype.Repository;
-
-
 import java.util.Optional;
-import java.util.UUID;
 
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findUserByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
