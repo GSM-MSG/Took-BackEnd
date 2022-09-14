0c36ba9cb05e6dcf91b6af5fe32bfa23f5fdd17a
Huuuunee <s21024@gsm.hs.kr>
2022-09-14 10:15:09 +0900

    :recycle: responseEntity사용

--- src/main/java/com/example/took_backend/domain/user/presentation/UserController.java
+++ src/main/java/com/example/took_backend/domain/user/presentation/UserController.java
@@ -3,6 +3,8 @@ package com.example.took_backend.domain.user.presentation;
 import com.example.took_backend.domain.user.presentation.dto.response.MyCardInfoResponse;
 import com.example.took_backend.domain.user.service.FindCardInfoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,7 @@ public class UserController {
     private final FindCardInfoService findCardInfoService;
 
     @GetMapping
-    public MyCardInfoResponse getMyCardInfo(){
-        return findCardInfoService.findCardInfo();
+    public ResponseEntity<MyCardInfoResponse> getMyCardInfo(){
+        return new ResponseEntity<>(findCardInfoService.findCardInfo(), HttpStatus.OK);
     }
 }
