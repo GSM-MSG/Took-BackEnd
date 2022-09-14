2c6ebfaaa260d2585fa9e31d79b4f8e35ae598c8
Huuuunee <s21024@gsm.hs.kr>
2022-09-14 23:40:02 +0900

    RequestParam이 아닌 RequestPart 로 변경

--- src/main/java/com/example/took_backend/domain/image/presentation/ImageController.java
+++ src/main/java/com/example/took_backend/domain/image/presentation/ImageController.java
@@ -1,14 +1,12 @@
 package com.example.took_backend.domain.image.presentation;
 
-import com.example.took_backend.domain.image.exception.FailedToUploadException;
 import com.example.took_backend.domain.image.service.ImageUploadService;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -22,8 +20,8 @@ public class ImageController {
     private final ImageUploadService imageUploadService;
 
     @PostMapping
-    public ResponseEntity<List<String>> uploadFile(@Valid @RequestParam("images")List<MultipartFile> multipartFilesList){
-        List<String> fileUrl = imageUploadService.upload(multipartFilesList).orElseThrow(()-> new FailedToUploadException("업로드 실패"));
+    public ResponseEntity<List<String>> uploadFile(@Valid @RequestPart(value = "files", required = false) List<MultipartFile> multipartFilesList){
+        List<String> fileUrl = imageUploadService.upload(multipartFilesList);
         return new ResponseEntity<>(fileUrl,HttpStatus.OK);
     }
 }
