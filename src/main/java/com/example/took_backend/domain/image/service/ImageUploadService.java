ad447c17f26b9ded7c2bc02fcbe01d4b1ff29e66
Huuuunee <s21024@gsm.hs.kr>
2022-09-14 23:23:56 +0900

    :sparkles: 이미지 업로드 기능 구현

---
+++ src/main/java/com/example/took_backend/domain/image/service/ImageUploadService.java
@@ -0,0 +1,53 @@
+package com.example.took_backend.domain.image.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.example.took_backend.domain.image.exception.FailedToUploadException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ImageUploadService {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.url}")
+    private String url;
+
+    private final AmazonS3 amazonS3;
+
+    public Optional<List<String>> upload(List<MultipartFile> multipartFile) {
+        List<String> fileNameList = new ArrayList<>();
+
+        multipartFile.forEach(file -> {
+            String fileName = createFileName(file.getOriginalFilename());
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+            try (InputStream inputStream = file.getInputStream()) {
+                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+
+            } catch (IOException e) {
+                throw new FailedToUploadException("파일 업로드에 실패했습니다.");
+            }
+            fileNameList.add(url + fileName);
+        });
+        return Optional.of(fileNameList);
+    }
+    private String createFileName(String fileName) {
+        return UUID.randomUUID().toString();
+    }
+}
