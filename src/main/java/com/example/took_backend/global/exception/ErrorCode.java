2a034e4b0e965943216ed52450ba03a99f3d1e1f
Huuuunee <s21024@gsm.hs.kr>
2022-09-15 17:57:29 +0900

    :recycle: ASC정렬

--- src/main/java/com/example/took_backend/global/exception/ErrorCode.java
+++ src/main/java/com/example/took_backend/global/exception/ErrorCode.java
@@ -13,9 +13,8 @@ public enum ErrorCode {
     TOKEN_NOT_VALID("토큰이 유효 하지 않습니다.", 401),
     USER_NOT_FOUND("유저를 찾을 수 없습니다.",404),
     PASSWORD_WRONG("비밀번호가 올바르지 않습니다.", 404),
-    EMAIL_ALREADY_EXIST("존재하는 이메일 입니다.", 409),
-
     CARD_NOT_FOUND("명함을 찾을 수 없습니다.",404),
+    EMAIL_ALREADY_EXIST("존재하는 이메일 입니다.", 409),
     MANY_REQUEST_EMAIL_AUTH("15분에 최대 3번 이메일 인증을 요청할 수 있습니다.", 429),
     FAILED_TO_UPLOAD("사진업로드를 실패했습니다",500),
     UNKNOWN("알 수 없는 에러", 500),
