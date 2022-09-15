06d213795dfe39f34226c417ceba438f8f547df5
Huuuunee <s21024@gsm.hs.kr>
2022-09-15 17:40:49 +0900

    :recycle: setter 제거 + validation 추가

--- src/main/java/com/example/took_backend/domain/auth/dto/request/UserSignInRequest.java
+++ src/main/java/com/example/took_backend/domain/auth/dto/request/UserSignInRequest.java
@@ -3,13 +3,14 @@ package com.example.took_backend.domain.auth.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import javax.validation.constraints.NotBlank;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserSignInRequest {
+    @NotBlank(message = "이메일(필수)")
     private String email;
+    @NotBlank(message = "비밀번호(필수)")
     private String password;
 }
--- src/main/java/com/example/took_backend/domain/auth/dto/request/UserSignUpRequest.java
+++ src/main/java/com/example/took_backend/domain/auth/dto/request/UserSignUpRequest.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserSignUpRequest {
+    @NotBlank(message = "이메일(필수)")
     private String email;
+    @NotBlank(message = "비밀번호(필수)")
     private String password;
 }
