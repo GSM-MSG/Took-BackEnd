f0f92c81f41b5eebe45afdb2fa317813d28bae8b
louis7308 <jswa7308@naver.com>
2022-09-15 15:13:09 +0900

    :sparkles: :: 로그인 Service Logic

---
+++ src/main/java/com/example/took_backend/domain/auth/service/SignInService.java
@@ -0,0 +1,43 @@
+package com.example.took_backend.domain.auth.service;
+
+import com.example.took_backend.domain.auth.RefreshTokenAuthEntity;
+import com.example.took_backend.domain.auth.dto.request.UserSignInRequest;
+import com.example.took_backend.domain.auth.dto.response.UserSignInResponse;
+import com.example.took_backend.domain.auth.exception.PasswordWrongExceptin;
+import com.example.took_backend.domain.auth.repository.RefreshTokenRepository;
+import com.example.took_backend.domain.user.User;
+import com.example.took_backend.domain.user.repository.UserRepository;
+import com.example.took_backend.global.exception.exceptionCollection.UserNotFoundException;
+import com.example.took_backend.global.security.jwt.TokenProvider;
+import com.example.took_backend.global.security.jwt.properties.JwtProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SignInService {
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+    private final JwtProperties jwtProperties;
+
+    public UserSignInResponse signIn(UserSignInRequest request) {
+        User user = userRepository.findUserByEmail(request.getEmail()).orElseThrow(() -> new UserNotFoundException("유저를 찾지 못했습니다."));
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new PasswordWrongExceptin("비밀번호가 올바르지 않습니다.");
+        }
+        String accessToken = tokenProvider.generatedAccessToken(request.getEmail());
+        String refreshToken = tokenProvider.generatedRefreshToken(request.getEmail());
+        RefreshTokenAuthEntity entityToRedis = new RefreshTokenAuthEntity(refreshToken, request.getEmail(), tokenProvider.getREFRESH_TOKEN_EXPIRE_TIME());
+        refreshTokenRepository.save(entityToRedis);
+        return UserSignInResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .expiredAt(tokenProvider.getExpiredAtToken(accessToken, jwtProperties.getAccessSecret()))
+                .build();
+    }
+}
