3b4700135c1022973b584708aa2886b9ee378bc8
louis7308 <jswa7308@naver.com>
2022-09-15 15:11:10 +0900

    :sparkles: :: 로그인 Controller

--- src/main/java/com/example/took_backend/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/took_backend/domain/auth/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.example.took_backend.domain.auth.controller;
 
+import com.example.took_backend.domain.auth.dto.request.UserSignInRequest;
 import com.example.took_backend.domain.auth.dto.request.UserSignUpRequest;
+import com.example.took_backend.domain.auth.dto.response.UserSignInResponse;
+import com.example.took_backend.domain.auth.service.SignInService;
 import com.example.took_backend.domain.auth.service.SignupService;
+import com.example.took_backend.domain.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final SignupService signupService;
+    private final SignInService signInService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@RequestBody UserSignUpRequest userReq) {
         signupService.execute(userReq);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<UserSignInResponse> signIn(@RequestBody UserSignInRequest userReq) {
+        UserSignInResponse data = signInService.signIn(userReq);
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
 }
