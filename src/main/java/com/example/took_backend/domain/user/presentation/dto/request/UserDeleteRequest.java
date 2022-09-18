94039881b8de00ebc0cd5f7b0d02346bfe6539ce
Huuuunee <s21024@gsm.hs.kr>
2022-09-18 23:18:19 +0900

    :fire: 필요없는 어노테이션 삭제

--- src/main/java/com/example/took_backend/domain/user/presentation/dto/request/UserDeleteRequest.java
+++ src/main/java/com/example/took_backend/domain/user/presentation/dto/request/UserDeleteRequest.java
@@ -3,9 +3,6 @@ package com.example.took_backend.domain.user.presentation.dto.request;
 import lombok.*;
 
 @Getter
-@Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class UserDeleteRequest {
     private String password;
 }
