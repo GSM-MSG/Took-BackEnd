d6d08d388535d94194cabc80338a020fc74b94df
Huuuunee <s21024@gsm.hs.kr>
2022-09-18 23:02:55 +0900

    :recycle: 공백 제거

--- src/main/java/com/example/took_backend/global/exception/ErrorCode.java
+++ src/main/java/com/example/took_backend/global/exception/ErrorCode.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     AUTH_CODE_MISMATCH("인증번호가 일치하지 않습니다,",400),
-
     NOT_VERIFY_EMAIL("이메일이 확인 되지 않음", 401),
     TOKEN_EXPIRATION("토큰이 만료 되었습니다.", 401),
     TOKEN_NOT_VALID("토큰이 유효 하지 않습니다.", 401),
