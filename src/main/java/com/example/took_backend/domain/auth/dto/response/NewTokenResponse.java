51f12be5447ca3945aa4cf35660847897453ebf8
louis7308 <jswa7308@naver.com>
2022-09-18 22:53:02 +0900

    :sparkles: :: 새 발급 Token Res dto

---
+++ src/main/java/com/example/took_backend/domain/auth/dto/response/NewTokenResponse.java
@@ -0,0 +1,18 @@
+package com.example.took_backend.domain.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NewTokenResponse {
+    private String accessToken;
+    private String refreshToken;
+    private Date expiredAt;
+}
