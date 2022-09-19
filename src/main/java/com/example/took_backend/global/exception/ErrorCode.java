578ad69d41c8cf8a36abda2b9cc2cf7bdf9e38b0
louis7308 <jswa7308@naver.com>
2022-09-19 17:11:54 +0900

    :sparkles: :: 사용하지 않는 ErrorCode 삭제

--- src/main/java/com/example/took_backend/global/exception/ErrorCode.java
+++ src/main/java/com/example/took_backend/global/exception/ErrorCode.java
@@ -11,8 +11,6 @@ public enum ErrorCode {
     NOT_VERIFY_EMAIL("이메일이 확인 되지 않음", 401),
     TOKEN_EXPIRATION("토큰이 만료 되었습니다.", 401),
     TOKEN_NOT_VALID("토큰이 유효 하지 않습니다.", 401),
-
-    TOKEN_NOT_FOUND("토큰을 찾을 수 없습니다", 404),
     USER_NOT_FOUND("유저를 찾을 수 없습니다.",404),
     PASSWORD_WRONG("비밀번호가 올바르지 않습니다.", 404),
     CARD_NOT_FOUND("명함을 찾을 수 없습니다.",404),
