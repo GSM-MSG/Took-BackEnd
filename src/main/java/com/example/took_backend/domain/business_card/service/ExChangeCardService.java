3b1d76d8fafb6b30b40ac7b458bc0ba9d4221ee8
louis7308 <jswa7308@naver.com>
2022-09-26 23:42:24 +0900

    :sparkles: :: 명함 교환 Service

--- src/main/java/com/example/took_backend/domain/business_card/service/ExChangeCardService.java
+++ src/main/java/com/example/took_backend/domain/business_card/service/ExChangeCardService.java
@@ -1,21 +1,41 @@
 package com.example.took_backend.domain.business_card.service;
 
+import com.example.took_backend.domain.business_card.entity.BusinessCard;
+import com.example.took_backend.domain.business_card.entity.CardExchange;
+import com.example.took_backend.domain.business_card.entity.CardExchangeId;
+import com.example.took_backend.domain.business_card.exception.BusinessCardNotFoundException;
+import com.example.took_backend.domain.business_card.presentation.dto.request.ExChangeBusinessCardRequest;
 import com.example.took_backend.domain.business_card.presentation.dto.response.ExchangeCardResponse;
+import com.example.took_backend.domain.business_card.repository.BusinessCardRepository;
+import com.example.took_backend.domain.business_card.repository.CardExchangeRepository;
 import com.example.took_backend.domain.user.entity.User;
+import com.example.took_backend.domain.user.exception.UserNotFoundException;
 import com.example.took_backend.domain.user.repository.UserRepository;
 import com.example.took_backend.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ExChangeCardService {
     private final UserUtil userUtil;
-    private final UserRepository userRepository;
-
-    public void BusineessCardExchange(String uuid) {
-        User currentUser = userUtil.currentUser();
+    private final CardExchangeRepository cardExchangeRepository;
+    private final BusinessCardRepository businessCardRepository;
+    public ExchangeCardResponse BusineessCardExchange(ExChangeBusinessCardRequest uuid) {
+        User currentUser = userUtil.currentUser(); // 사용자가 있어요~
+        BusinessCard businessCard = businessCardRepository.findById(uuid.getUuid())
+                .orElseThrow(() -> new BusinessCardNotFoundException("명함을 찾을 수 없습니다."));
+        if(businessCard.getUser().getUuid().isEmpty()) {
+            throw new UserNotFoundException("교환할 상대방을 찾을 수 없습니다.");
+        }
+        CardExchange cardExchange = CardExchange.builder()
+                .user(currentUser)
+                .businessUser(businessCard.getUser())
+                .businessCard(businessCard)
+                .build();
+        cardExchangeRepository.save(cardExchange);
+        ExchangeCardResponse exchangeCardResponse = ExchangeCardResponse.builder()
+                .uuid()
 
     }
 }
